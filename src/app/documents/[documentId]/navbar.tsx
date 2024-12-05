8ef7221a05fd79f71307734fba102334198605b1
narandza <60319154+narandza@users.noreply.github.com>
2024-12-05 09:08:18 +0100

    create inital document input component

--- src/app/documents/[documentId]/navbar.tsx
+++ src/app/documents/[documentId]/navbar.tsx
@@ -1,9 +1,19 @@
 import Image from "next/image";
+import Link from "next/link";
+import { DocumentInput } from "./document-input";
 
 export const Navbar = () => {
   return (
-    <nav className="">
-      <Image src="/logo.svg" alt="logo" width={36} height={36} />
+    <nav className="flex items-center justify-between">
+      <div className="flex gap-2 items-center">
+        <Link href="/">
+          <Image src="/logo.svg" alt="logo" width={36} height={36} />
+        </Link>
+        <div className="flex flex-col">
+          <DocumentInput />
+          {/* Menu bar  */}
+        </div>
+      </div>
     </nav>
   );
 };
