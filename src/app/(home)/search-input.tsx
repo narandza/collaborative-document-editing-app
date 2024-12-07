02b6cd7da953d2438b0858ccff16f5bf32f3fb27
narandza <60319154+narandza@users.noreply.github.com>
2024-12-07 22:12:39 +0100

    add state control to search input

--- src/app/(home)/search-input.tsx
+++ src/app/(home)/search-input.tsx
@@ -1,12 +1,31 @@
+"use client";
+
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
-import { SearchIcon } from "lucide-react";
+import { SearchIcon, XIcon } from "lucide-react";
+import { useRef, useState } from "react";
 
 export const SearchInput = () => {
+  const [value, setValue] = useState("");
+
+  const inputRef = useRef<HTMLInputElement>(null);
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setValue(e.target.value);
+  };
+
+  const handleClear = () => {
+    setValue("");
+    inputRef.current?.blur();
+  };
+
   return (
     <div className="flex-1 flex items-center justify-center">
       <form className="relative max-w-[720px] w-full">
         <Input
+          onChange={handleChange}
+          ref={inputRef}
+          value={value}
           placeholder="Search"
           className="md:text-base placeholder:text-neutral-800 px-14 w-full border-none focus-visible:shadow-[0_1px_1px_0_rgba(65,69,73,.3),0_1px_3px_1px_rgba(65,69,73,.15)] bg-[#f0f4f8] rounded-full h-[48px] focus-visible:ring-0 focus:bg-white"
         />
@@ -18,6 +37,17 @@ export const SearchInput = () => {
         >
           <SearchIcon />
         </Button>
+        {value && (
+          <Button
+            onClick={handleClear}
+            type="button"
+            variant="ghost"
+            size="icon"
+            className="absolute right-3 top-1/2 -translate-y-1/2 [&_svg]:size-5 rounded-full"
+          >
+            <XIcon />
+          </Button>
+        )}
       </form>
     </div>
   );
