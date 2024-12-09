2a1d6a9b622dc554daaf5a2f6762060a73c13926
narandza <60319154+narandza@users.noreply.github.com>
2024-12-09 09:19:35 +0100

    create documents table layout

--- src/app/(home)/documents-table.tsx
+++ src/app/(home)/documents-table.tsx
@@ -1,5 +1,14 @@
 import { PaginationStatus } from "convex/react";
 import { Doc } from "../../../convex/_generated/dataModel";
+import { LoaderIcon } from "lucide-react";
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table";
 
 interface DocumentsTableProps {
   documents: Doc<"documents">[] | undefined;
@@ -12,5 +21,38 @@ export const DocumentsTable = ({
   status,
   loadMore,
 }: DocumentsTableProps) => {
-  return <div className="">doc table</div>;
+  return (
+    <div className="max-w-screen-xl mx-auto px-16 py-6 flex flex-col gap-5">
+      {documents === undefined ? (
+        <div className="flex justify-center items-center h-24">
+          <LoaderIcon className="animate-spin text-muted-foreground size-5" />
+        </div>
+      ) : (
+        <Table>
+          <TableHeader>
+            <TableRow className="hover:bg-transparent border-none">
+              <TableHead>Name</TableHead>
+              <TableHead>&nbsp;</TableHead>
+              <TableHead>Shared</TableHead>
+              <TableHead>Created at</TableHead>
+            </TableRow>
+          </TableHeader>
+          {documents.length === 0 ? (
+            <TableBody>
+              <TableRow className="hover:bg-transparent">
+                <TableCell
+                  colSpan={4}
+                  className="h-24 text-center text-muted-foreground"
+                >
+                  No documents found
+                </TableCell>
+              </TableRow>
+            </TableBody>
+          ) : (
+            <TableBody>Documents</TableBody>
+          )}
+        </Table>
+      )}
+    </div>
+  );
 };
