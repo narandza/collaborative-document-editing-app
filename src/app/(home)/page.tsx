9fd10c5020ae3be860d9ec26cef6ab23f6649cc2
narandza <60319154+narandza@users.noreply.github.com>
2024-12-09 08:10:44 +0100

    finish documents schema

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,7 +1,17 @@
+"use client";
+
+import { useQuery } from "convex/react";
 import { Navbar } from "./navbar";
 import { TemplateGallery } from "./template-gallery";
+import { api } from "../../../convex/_generated/api";
 
 const Home = () => {
+  const documents = useQuery(api.documents.get);
+
+  if (documents === undefined) {
+    return <p className="">loading</p>;
+  }
+
   return (
     <div className="min-h-screen flex flex-col">
       <div className="fixed top-0 left-0 right-0 z-10 h-16 bg-white p-4">
@@ -9,6 +19,9 @@ const Home = () => {
       </div>
       <div className="mt-16">
         <TemplateGallery />
+        {documents?.map((document) => (
+          <p key={document._id}>{document.title}</p>
+        ))}
       </div>
     </div>
   );
