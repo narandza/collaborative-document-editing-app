e45bc22502106cbbded3c3c7c8f5e4bf9500e532
narandza <60319154+narandza@users.noreply.github.com>
2024-12-13 10:34:15 +0100

    QoL update

--- convex/documents.ts
+++ convex/documents.ts
@@ -99,7 +99,9 @@ export const removeById = mutation({
     }
 
     const isOwner = document.ownerId === user.subject;
-    const isOrganizationMember = document.organizationId === organizationId;
+    const isOrganizationMember = !!(
+      document.organizationId && document.organizationId === organizationId
+    );
 
     if (!isOwner && !isOrganizationMember) {
       throw new ConvexError("Unauthorized");
@@ -133,7 +135,9 @@ export const updateById = mutation({
 
     const isOwner = document.ownerId === user.subject;
 
-    const isOrganizationMember = document.organizationId === organizationId;
+    const isOrganizationMember = !!(
+      document.organizationId && document.organizationId === organizationId
+    );
 
     if (!isOwner && !isOrganizationMember) {
       throw new ConvexError("Unauthorized");
--- src/app/documents/[documentId]/room.tsx
+++ src/app/documents/[documentId]/room.tsx
@@ -12,11 +12,7 @@ export function Room({ children }: { children: ReactNode }) {
   const params = useParams();
 
   return (
-    <LiveblocksProvider
-      publicApiKey={
-        "pk_dev_LKmhtuoVHZofIxxime8eSMWZY7yGX5M9kfDgUwRvT8fNMa79_rTRCuyRL6fbIlus"
-      }
-    >
+    <LiveblocksProvider throttle={16} authEndpoint="/api/liveblocks-auth">
       <RoomProvider id={params.documentId as string}>
         <ClientSideSuspense fallback={<div>Loading…</div>}>
           {children}
