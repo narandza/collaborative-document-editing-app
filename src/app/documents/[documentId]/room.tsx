31cf3378d88916b67adfa660657ea605d9b33c98
narandza <60319154+narandza@users.noreply.github.com>
2024-12-15 12:33:52 +0100

    creatre docId server actions; modify code

--- src/app/documents/[documentId]/room.tsx
+++ src/app/documents/[documentId]/room.tsx
@@ -1,20 +1,33 @@
 "use client";
 
-import { ReactNode } from "react";
+import { ReactNode, useState } from "react";
 import {
   LiveblocksProvider,
   RoomProvider,
   ClientSideSuspense,
 } from "@liveblocks/react/suspense";
 import { useParams } from "next/navigation";
+import { FullScreenLoader } from "@/components/full-screen-loader";
+
+type User = { id: string; name: string; avatar: string };
 
 export function Room({ children }: { children: ReactNode }) {
   const params = useParams();
 
+  const [users, setUsers] = useState<User[]>([]);
+
   return (
-    <LiveblocksProvider throttle={16} authEndpoint="/api/liveblocks-auth">
+    <LiveblocksProvider
+      throttle={16}
+      authEndpoint="/api/liveblocks-auth"
+      resolveUsers={() => []}
+      resolveMentionSuggestions={() => []}
+      resolveRoomsInfo={() => []}
+    >
       <RoomProvider id={params.documentId as string}>
-        <ClientSideSuspense fallback={<div>Loading…</div>}>
+        <ClientSideSuspense
+          fallback={<FullScreenLoader label="Room loading..." />}
+        >
           {children}
         </ClientSideSuspense>
       </RoomProvider>
