6578cb7895772a894ae850abd775d1db4ff02782
narandza <60319154+narandza@users.noreply.github.com>
2024-12-17 19:29:11 +0100

    create loading page

---
+++ src/app/documents/[documentId]/loading.tsx
@@ -0,0 +1,7 @@
+import { FullScreenLoader } from "@/components/full-screen-loader";
+
+const LoadingPage = () => {
+  return <FullScreenLoader label="Document loading..." />;
+};
+
+export default LoadingPage;
