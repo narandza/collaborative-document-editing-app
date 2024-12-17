3c06e10c8e748bd91cdee48fef7ddf9fdb36378b
narandza <60319154+narandza@users.noreply.github.com>
2024-12-17 19:52:49 +0100

    create on chnage title

--- src/app/documents/[documentId]/document-input.tsx
+++ src/app/documents/[documentId]/document-input.tsx
@@ -1,14 +1,59 @@
 import { BsCloudCheck } from "react-icons/bs";
 import { Id } from "../../../../convex/_generated/dataModel";
+import { useRef, useState } from "react";
+import { useMutation } from "convex/react";
+import { api } from "../../../../convex/_generated/api";
 
 interface DocumentInputProps {
   title: string;
   id: Id<"documents">;
 }
 export const DocumentInput = ({ title, id }: DocumentInputProps) => {
+  const [value, setValue] = useState(title);
+  const [isError, setIsError] = useState(false);
+  const [isPending, setIsPending] = useState(false);
+  const [isEditing, setIsEditing] = useState(false);
+
+  const inputRef = useRef<HTMLInputElement>(null);
+
+  const mutate = useMutation(api.documents.updateById);
+
+  const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const newValue = e.target.value;
+
+    setValue(newValue);
+
+    // TODO: Debounced value
+  };
+
   return (
     <div className="flex items-center gap-2">
-      <span className="text-lg px-1.5 cursor-pointer truncate">{title}</span>
+      {isEditing ? (
+        <form className="relative w-fit max-w-[50ch]">
+          <span className="invisible whitespace-pre px-1.5 text-lg">
+            {value || " "}
+          </span>
+          <input
+            ref={inputRef}
+            onChange={onChange}
+            value={value}
+            className="absolute inset-0 text-lg text-black px-1.5 bg-transparent truncate"
+          />
+        </form>
+      ) : (
+        <span
+          onClick={() => {
+            setIsEditing(true);
+            setTimeout(() => {
+              inputRef.current?.focus();
+            }, 0);
+          }}
+          className="text-lg px-1.5 cursor-pointer truncate"
+        >
+          {title}
+        </span>
+      )}
+
       <BsCloudCheck />
     </div>
   );
