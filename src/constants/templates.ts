ec4cf4d981c9799e2972d224301b1b18f27d830b
narandza <60319154+narandza@users.noreply.github.com>
2025-01-04 22:11:49 +0100

    improve project proposal template

--- src/constants/templates.ts
+++ src/constants/templates.ts
@@ -53,15 +53,48 @@ export const templates = [
     label: "Project proposal",
     imageUrl: "/project-proposal.svg",
     initialContent: `
-    <h1>Project Proposal</h1>
-    <h2>Executive Summary</h2>
-    <p>Brief overview of the project proposal.</p>
-
-    <h2>Project Goals</h2>
-    <p>Strategy and methodology for project execution</p>
-
-    <h2>Resources Required</h2>
-    <p>Team, equipment, and requirements</p>
+   <h1>Project Proposal</h1>
+    <p>Submitted by: [Your Name or Company Name]</p>
+    <p>Date: [Insert Date]</p>
+    
+    <h2>Project Title</h2>
+    <p>[Insert the title of the project]</p>
+    
+    <h2>Client or Stakeholder Information</h2>
+    <p><strong>[Client or Stakeholder Name]</strong></p>
+    <p>[Client or Stakeholder Title]</p>
+    <p>[Organization Name]</p>
+    <p>Email: [Email Address] | Phone: [Phone Number]</p>
+    
+    <h2>Introduction</h2>
+    <p>[Provide a brief introduction to the project, including the purpose and background information.]</p>
+    
+    <h2>Project Objectives</h2>
+    <p>[List the specific objectives you aim to achieve with this project.]</p>
+    
+    <h2>Scope of Work</h2>
+    <p>[Describe the scope of the project, including what is and is not included in the work to be performed.]</p>
+    
+    <h2>Methodology</h2>
+    <p>[Outline the approach or methodology that will be used to complete the project successfully.]</p>
+    
+    <h2>Timeline</h2>
+    <p>[Provide a timeline with key milestones and deadlines for project completion.]</p>
+    
+    <h2>Budget</h2>
+    <p>[Include a budget breakdown detailing costs associated with the project, such as labor, materials, and other expenses.]</p>
+    
+    <h2>Expected Outcomes</h2>
+    <p>[Highlight the expected outcomes or deliverables of the project.]</p>
+    
+    <h2>Conclusion</h2>
+    <p>[Summarize the proposal and reinforce the value of the project. Express your enthusiasm for collaboration.]</p>
+    
+    <h2>Contact Information</h2>
+    <p><strong>[Your Name]</strong></p>
+    <p>[Your Title]</p>
+    <p>[Your Organization Name]</p>
+    <p>Email: [Your Email] | Phone: [Your Phone]</p>
 `,
   },
   {
